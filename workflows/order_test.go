9fceaea3056e741f45d6e0f3a6946156ca3498d9
Rob Holland <rob.holland@gmail.com>
2023-11-22 11:27:38 +0000

    Add refund test.

--- workflows/order_test.go
+++ workflows/order_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/temporalio/temporal-cafe/activities"
+	act "github.com/temporalio/temporal-cafe/activities"
 	"github.com/temporalio/temporal-cafe/proto"
 	"github.com/temporalio/temporal-cafe/workflows"
 	"go.temporal.io/sdk/activity"
@@ -16,18 +16,18 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-var a *activities.Activities
+var activities *act.Activities
 
 func TestOrderWorkflow(t *testing.T) {
 	s := testsuite.WorkflowTestSuite{}
 	env := s.NewTestWorkflowEnvironment()
 
 	env.RegisterWorkflow(workflows.Order)
-	env.RegisterActivity(a.ProcessPayment)
-	env.RegisterActivity(a.ProcessPaymentRefund)
+	env.RegisterActivity(activities.ProcessPayment)
+	env.RegisterActivity(activities.ProcessPaymentRefund)
 	env.RegisterWorkflow(workflows.KitchenOrder)
 	env.RegisterWorkflow(workflows.BaristaOrder)
-	env.RegisterActivity(a.AddLoyaltyPoints)
+	env.RegisterActivity(activities.AddLoyaltyPoints)
 
 	input := &proto.OrderInput{
 		Email:        "test@example.com",
@@ -89,15 +89,15 @@ func TestOrderWorkflow(t *testing.T) {
 		}
 	})
 
-	env.OnActivity(a.ProcessPayment, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentInput) (*proto.ProcessPaymentResult, error) {
+	env.OnActivity(activities.ProcessPayment, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentInput) (*proto.ProcessPaymentResult, error) {
 		return &proto.ProcessPaymentResult{}, nil
 	})
 
-	env.OnActivity(a.ProcessPaymentRefund, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentRefundInput) (*proto.ProcessPaymentRefundResult, error) {
+	env.OnActivity(activities.ProcessPaymentRefund, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentRefundInput) (*proto.ProcessPaymentRefundResult, error) {
 		return &proto.ProcessPaymentRefundResult{}, nil
 	})
 
-	env.OnActivity(a.AddLoyaltyPoints, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.AddLoyaltyPointsInput) (*proto.AddLoyaltyPointsResult, error) {
+	env.OnActivity(activities.AddLoyaltyPoints, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.AddLoyaltyPointsInput) (*proto.AddLoyaltyPointsResult, error) {
 		return &proto.AddLoyaltyPointsResult{}, nil
 	})
 
@@ -127,8 +127,8 @@ func TestOrderWorkflowFulfilmentDeadline(t *testing.T) {
 	env := s.NewTestWorkflowEnvironment()
 
 	env.RegisterWorkflow(workflows.Order)
-	env.RegisterActivity(a.ProcessPayment)
-	env.RegisterActivity(a.ProcessPaymentRefund)
+	env.RegisterActivity(activities.ProcessPayment)
+	env.RegisterActivity(activities.ProcessPaymentRefund)
 	env.RegisterWorkflow(workflows.KitchenOrder)
 	env.RegisterWorkflow(workflows.BaristaOrder)
 
@@ -156,11 +156,11 @@ func TestOrderWorkflowFulfilmentDeadline(t *testing.T) {
 		}
 	})
 
-	env.OnActivity(a.ProcessPayment, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentInput) (*proto.ProcessPaymentResult, error) {
+	env.OnActivity(activities.ProcessPayment, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentInput) (*proto.ProcessPaymentResult, error) {
 		return &proto.ProcessPaymentResult{}, nil
 	})
 
-	env.OnActivity(a.ProcessPaymentRefund, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentRefundInput) (*proto.ProcessPaymentRefundResult, error) {
+	env.OnActivity(activities.ProcessPaymentRefund, mock.Anything, mock.Anything).Return(func(ctx context.Context, input *proto.ProcessPaymentRefundInput) (*proto.ProcessPaymentRefundResult, error) {
 		return &proto.ProcessPaymentRefundResult{}, nil
 	})
 
@@ -181,6 +181,51 @@ func TestOrderWorkflowFulfilmentDeadline(t *testing.T) {
 
 	err := env.GetWorkflowResult(&result)
 	assert.Error(t, fmt.Errorf("order not fulfilled within window"), err)
+
+	assert.Equal(t, expectedCalls, activityCalls)
+}
+
+func TestOrderWorkflowRefund(t *testing.T) {
+	s := testsuite.WorkflowTestSuite{}
+	env := s.NewTestWorkflowEnvironment()
+
+	env.RegisterWorkflow(workflows.Order)
+	env.RegisterActivity(activities.ProcessPayment)
+	env.RegisterActivity(activities.ProcessPaymentRefund)
+
+	input := &proto.OrderInput{
+		PaymentToken: "x",
+		Items: []*proto.OrderLineItem{
+			{Type: proto.ProductType_PRODUCT_TYPE_BEVERAGE, Name: "coffee", Count: 1},
+			{Type: proto.ProductType_PRODUCT_TYPE_BEVERAGE, Name: "latte", Count: 2},
+			{Type: proto.ProductType_PRODUCT_TYPE_FOOD, Name: "bagel", Count: 2},
+		},
+	}
+
+	env.OnWorkflow(workflows.BaristaOrder, mock.Anything, mock.Anything).Return(func(ctx workflow.Context, input *proto.BaristaOrderInput) (*proto.BaristaOrderResult, error) {
+		return &proto.BaristaOrderResult{}, fmt.Errorf("failed")
+	})
+	env.OnWorkflow(workflows.KitchenOrder, mock.Anything, mock.Anything).Return(func(ctx workflow.Context, input *proto.KitchenOrderInput) (*proto.KitchenOrderResult, error) {
+		return &proto.KitchenOrderResult{}, nil
+	})
+
+	var activityCalls []string
+	env.SetOnActivityStartedListener(func(activityInfo *activity.Info, ctx context.Context, args converter.EncodedValues) {
+		activityCalls = append(activityCalls, activityInfo.ActivityType.Name)
+	})
+
+	expectedCalls := []string{
+		"ProcessPayment",
+		"ProcessPaymentRefund",
+	}
+
+	env.ExecuteWorkflow(workflows.Order, input)
+	assert.True(t, env.IsWorkflowCompleted())
+
+	var result proto.OrderResult
+
+	err := env.GetWorkflowResult(&result)
+	assert.Error(t, err)
 
 	assert.Equal(t, expectedCalls, activityCalls)
 }
